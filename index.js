a665dfccbe5294c97f74a1e796d76535bb2a6d54
kamron-dev <k.kamron@yadnex.ru>
2024-08-12 02:36:02 +0500

    feature(game card) card updates on click filling columns

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
 const cRows = document.querySelector("#cRows");
 const aRows = document.querySelector("#aRows");
 const kRows = document.querySelector("#kRows");
+const dateHolder = document.querySelector("#date");
+const today = new Date();
 
+dateHolder.textContent = today.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+});
 
 const totalCost = {
-    C: 0,
-    A: 0,
-    K: 0
+    C: 80,
+    A: 88,
+    K: 92
 };
 
 (function makeRowsWork() {
     
     const allTdElements = document.querySelectorAll(".game-cell");
 
-    allTdElements.forEach(element => element.addEventListener("dblclick", () => {
-        element.textContent = "X";
+    allTdElements.forEach((element, index) => element.addEventListener("click", (event) => {
+        // element.textContent = "X";
+        event.target.textContent = "X";
+        if (index >= 0 && index < 20) {
+            allTdElements[index + 20].textContent = "O";
+            allTdElements[index + 40].textContent = "O";
+        } else if (index >= 20 && index < 40) {
+            allTdElements[index - 20].textContent = "O";
+            allTdElements[index + 20].textContent = "O";
+        } else {
+            allTdElements[index - 20].textContent = "O";
+            allTdElements[index - 40].textContent = "O";
+        }
         updateTotal();
     }));
-    allTdElements.forEach(element => element.addEventListener("click", () => {
-        element.textContent = "O";
-        updateTotal();
-    }))
+    // allTdElements.forEach(element => element.addEventListener("click", () => {
+    //     element.textContent = "O";
+    //     updateTotal();
+    // }))
     
     
     
